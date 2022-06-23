ea5f35d06969f6585299ffa5f00ac3c45523c690
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-06-23 14:28:50 -0400

    e
    e

---
+++ lin-reg/lin-reg.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from scipy import stats
+
+x = [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6]
+y = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
+
+slope, intercept, relation, p, std_err = stats.linregress(x, y)
+
+
+def graph(x):
+    return slope * x + intercept
+
+model = list(map(graph, x))
+plt.scatter(x, y)
+plt.plot(x, model)
+plt.show()
