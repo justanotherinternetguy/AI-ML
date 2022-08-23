0748408b9687af841e0cadaaa38165799de05317
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-08-22 21:19:29 -0400

    e

--- RNN/README.md
+++ RNN/README.md
@@ -6,3 +6,27 @@
 # Usages
 * Machine translation
 * Sentiment analysis
+
+# How does this work?
+* Consider "many-to-many" RNN with inputs [x0, x1, ..., xn] that wants to produce outputs [y0, y1, ..., yn]
+  * xi and yi are vectors and can have arbitrary dimensions
+
+* RNNs iteratively update a hidden state h, which is a vector that can also have an arbitrary dimension.
+
+* At any given step t,
+  1. the next hidden state ht is calculated using the previous hidden state ht-1 and the next input xt
+  2. the next output yt is calculated using ht
+
+* RNNs use the **same weight each step**
+  * Wxh for all xt --> ht links
+  * Whh for all ht-1 --> ht links
+  * Why for all ht --> yt links
+
+  * Bh added when calculating ht
+  * By added when calcuating yt
+
+* Weights: matrices
+* Biases: vectors
+
+* ht = tanh(Wxh * xt + Whh * ht-1 + Bh)
+* yt = Why * h1 + By
---
+++ RNN/main.py
@@ -0,0 +1,33 @@
+from dataset import train_data, test_data
+
+# pre-processing of data
+
+vocab = list(set([w for text in train_data.keys() for w in text.split(' ')]))
+vocab_size = len(vocab)
+print('%d unique words found' % vocab_size)
+
+# assign index to each word
+word_to_idx = { w: i for i, w in enumerate(vocab) }
+idx_to_word = { i: w for i, w in enumerate(vocab) }
+print(word_to_idx['good'])
+print(idx_to_word[0])
+
+# each xi is a vector (one-hot vector - all contain 0s except for a single 1)
+# 1 will be at the word's corresponding integer index
+# we have 18 unique words = xi is a 18-dimensional one-hot
+
+import numpy as np
+
+def createInputs(text):
+  """
+  return an array of one-hot vectors representing the words in the input text string
+  - text in a string
+  - each one-hot has shape (vocab_size, 1)
+  """
+  
+  inputs = []
+  for w in text.split(' '):
+    v = np.zeros((vocab_size, 1))
+    v[word_to_idx[w]] = 1
+    inputs.append(v)
+  return inputs
