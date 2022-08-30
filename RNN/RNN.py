ef4f341e5cd3f3ac8f4727b967efb6e7e47a26d1
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-08-29 20:53:00 -0400

    e

--- RNN/RNN.py
+++ RNN/RNN.py
@@ -3,42 +3,84 @@ from numpy.random import randn
 
 
 class RNN:
+    # A many-to-one Vanilla Recurrent Neural Network.
+
     def __init__(self, input_size, output_size, hidden_size=64):
-        # weights
+        # Weights
         self.Whh = randn(hidden_size, hidden_size) / 1000
         self.Wxh = randn(hidden_size, input_size) / 1000
         self.Why = randn(output_size, hidden_size) / 1000
 
-        # biases
+        # Biases
         self.bh = np.zeros((hidden_size, 1))
         self.by = np.zeros((output_size, 1))
 
-    # RNN's forward pass with tanh
     def forward(self, inputs):
-        """
-        Returns the final output and the hidden state
-        - inputs: array of one-hot vectors as shape: (input_size, 1)
-        """
+        '''
+        Perform a forward pass of the RNN using the given inputs.
+        Returns the final output and hidden state.
+        - inputs is an array of one hot vectors with shape (input_size, 1).
+        '''
         h = np.zeros((self.Whh.shape[0], 1))
 
         self.last_inputs = inputs
         self.last_hs = {0: h}
 
-        # perform each step of RNN
+        # Perform each step of the RNN
         for i, x in enumerate(inputs):
             h = np.tanh(self.Wxh @ x + self.Whh @ h + self.bh)
+            self.last_hs[i + 1] = h
 
-            # output
-            y = self.Why @ h + self.by
+        # Compute the output
+        y = self.Why @ h + self.by
 
-            return y, h
+        return y, h
 
-        """
-    Cross-Entropy Loss (often paired w/ Sofmax)
-    L = -ln(pc)
-    pc is our RNN's predicted probability for the CORRECT classification
-    E.g if a postitive text is predicted to be 90% positive:
-      - L = -ln(0.9) = 0.105
-      
-    Now that there's a loss, we train RNN using gradient descent
-    """
+    def backprop(self, d_y, learn_rate=2e-2):
+        '''
+        Perform a backward pass of the RNN.
+        - d_y (dL/dy) has shape (output_size, 1).
+        - learn_rate is a float.
+        '''
+        n = len(self.last_inputs)
+
+        # Calculate dL/dWhy and dL/dby.
+        d_Why = d_y @ self.last_hs[n].T
+        d_by = d_y
+
+        # Initialize dL/dWhh, dL/dWxh, and dL/dbh to zero.
+        d_Whh = np.zeros(self.Whh.shape)
+        d_Wxh = np.zeros(self.Wxh.shape)
+        d_bh = np.zeros(self.bh.shape)
+
+        # Calculate dL/dh for the last h.
+        # dL/dh = dL/dy * dy/dh
+        d_h = self.Why.T @ d_y
+
+        # Backpropagate through time.
+        for t in reversed(range(n)):
+            # An intermediate value: dL/dh * (1 - h^2)
+            temp = ((1 - self.last_hs[t + 1] ** 2) * d_h)
+
+            # dL/db = dL/dh * (1 - h^2)
+            d_bh += temp
+
+            # dL/dWhh = dL/dh * (1 - h^2) * h_{t-1}
+            d_Whh += temp @ self.last_hs[t].T
+
+            # dL/dWxh = dL/dh * (1 - h^2) * x
+            d_Wxh += temp @ self.last_inputs[t].T
+
+            # Next dL/dh = dL/dh * (1 - h^2) * Whh
+            d_h = self.Whh @ temp
+
+        # Clip to prevent exploding gradients.
+        for d in [d_Wxh, d_Whh, d_Why, d_bh, d_by]:
+            np.clip(d, -1, 1, out=d)
+
+        # Update weights and biases using gradient descent.
+        self.Whh -= learn_rate * d_Whh
+        self.Wxh -= learn_rate * d_Wxh
+        self.Why -= learn_rate * d_Why
+        self.bh -= learn_rate * d_bh
+        self.by -= learn_rate * d_by
--- RNN/main.py
+++ RNN/main.py
@@ -1,31 +1,27 @@
 import numpy as np
-from dataset import train_data, test_data
+import random
+
 from rnn import RNN
+from dataset import train_data, test_data
 
-# pre-processing of data
-
+# Create the vocabulary.
 vocab = list(set([w for text in train_data.keys() for w in text.split(' ')]))
 vocab_size = len(vocab)
 print('%d unique words found' % vocab_size)
 
-# assign index to each word
+# Assign indices to each word.
 word_to_idx = {w: i for i, w in enumerate(vocab)}
 idx_to_word = {i: w for i, w in enumerate(vocab)}
-print(word_to_idx['good'])
-print(idx_to_word[0])
-
-# each xi is a vector (one-hot vector - all contain 0s except for a single 1)
-# 1 will be at the word's corresponding integer index
-# we have 18 unique words = xi is a 18-dimensional one-hot
+# print(word_to_idx['good'])
+# print(idx_to_word[0])
 
 
 def createInputs(text):
-    """
-    return an array of one-hot vectors representing the words in the input text string
-    - text in a string
-    - each one-hot has shape (vocab_size, 1)
-    """
-
+    '''
+    Returns an array of one-hot vectors representing the words in the input text string.
+    - text is a string
+    - Each one-hot vector has shape (vocab_size, 1)
+    '''
     inputs = []
     for w in text.split(' '):
         v = np.zeros((vocab_size, 1))
@@ -35,13 +31,56 @@ def createInputs(text):
 
 
 def softmax(xs):
-    # softmax func to input arr
+    # Applies the Softmax Function to the input array.
     return np.exp(xs) / sum(np.exp(xs))
 
 
+# Initialize our RNN!
 rnn = RNN(vocab_size, 2)
 
-inputs = createInputs('i am very good')
-out, h = rnn.forward(inputs)
-probs = softmax(out)
-print(probs)  # [[0.49999588] [0.50000412]]
+
+def processData(data, backprop=True):
+    '''
+    Returns the RNN's loss and accuracy for the given data.
+    - data is a dictionary mapping text to True or False.
+    - backprop determines if the backward phase should be run.
+    '''
+    items = list(data.items())
+    random.shuffle(items)
+
+    loss = 0
+    num_correct = 0
+
+    for x, y in items:
+        inputs = createInputs(x)
+        target = int(y)
+
+        # Forward
+        out, _ = rnn.forward(inputs)
+        probs = softmax(out)
+
+        # Calculate loss / accuracy
+        loss -= np.log(probs[target])
+        num_correct += int(np.argmax(probs) == target)
+
+        if backprop:
+            # Build dL/dy
+            d_L_d_y = probs
+            d_L_d_y[target] -= 1
+
+            # Backward
+            rnn.backprop(d_L_d_y)
+
+    return loss / len(data), num_correct / len(data)
+
+
+# Training loop
+for epoch in range(1000):
+    train_loss, train_acc = processData(train_data)
+
+    if epoch % 100 == 99:
+        print('--- Epoch %d' % (epoch + 1))
+        print('Train:\tLoss %.3f | Accuracy: %.3f' % (train_loss, train_acc))
+
+        test_loss, test_acc = processData(test_data, backprop=False)
+        print('Test:\tLoss %.3f | Accuracy: %.3f' % (test_loss, test_acc))
