171e593ef5eb525722464e50e7c66d2bd2bdd359
justanotherinternetguy <hendryxu1@gmail.com>
2022-12-29 15:53:16 -0500

    e

--- lin-reg/main.py
+++ lin-reg/main.py
@@ -1,20 +1,32 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 from scipy import stats
 
-x = [0.4, 1, 3, 7.2, 10, 12, 26.6, 26.8, 28, 28.4, 38, 42.4, 43.4, 47.9, 55, 56.2, 65]
-y = [0, 175.44, 315.79, 654.39, 857.90, 1066.67, 1500, 1850.88, 2010.53, 2140.36, 2756.15, 2887.738, 2945.63, 3107.03, 3571.94, 3966.68, 4352.64]
-
-slope, intercept, relation, p, std_err = stats.linregress(x, y)
+X = np.random.randint(100, size=100)
+Y = np.random.randint(100, size=100)
 
 
-def graph(x):
-  return slope * x + intercept
+x_mean = np.mean(X)
+y_mean = np.mean(Y);
+n = len(X)
 
-model = list(map(graph, x))
+numerator = 0
+denominator = 0
+for i in range(n):
+    numerator += (X[i] - x_mean) * (Y[i] - y_mean)
+    denominator += (X[i] - x_mean) ** 2
 
-print("SLOPE: {0}".format(slope))
 
-plt.scatter(x, y)
-plt.plot(x, model)
+b1 = numerator / denominator
+b0 = y_mean - (b1 * x_mean)
+
+x_max = np.max(X) + 100
+x_min = np.min(X) - 100
+
+x = np.linspace(x_min, x_max, 1000)
+y = b0 + b1 * x
+
+plt.plot(x, y, color='#00ff00')
+plt.scatter(X, Y)
 plt.show()
-
